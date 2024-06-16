36714ab0873ecdf003081ce81fbfc17ca4be90f3
Tobias <tobias.jansson@gmail.com>
2024-06-16 22:23:11 +0200

    Cleaning up, removing socketio

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,7 +1,6 @@
 import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
 import { GaragePortHomebridgePlatform } from './platform.js';
 import mqttjs, { MqttClient } from 'mqtt';
-//import { io, Socket } from 'socket.io-client';
 
 /**
  * Platform Accessory
@@ -10,8 +9,6 @@ import mqttjs, { MqttClient } from 'mqtt';
  */
 export class GarageDoorOpener {
   private service: Service;
-  // private socket: Socket;
-  // private url:string;
   private mqtt: MqttClient;
   private currentDoorState: CharacteristicValue;
   private targetDoorState: CharacteristicValue;
@@ -22,16 +19,13 @@ export class GarageDoorOpener {
   ) {
 
     this.platform.log.debug('GarageDoorOpener starting');
-    // this.url = 'https://mqtt.taklamakan.se';
-
     this.mqtt = this.setupMqtt();
-    // this.socket = this.SetupSocketIO();
 
     const characteristic = this.platform.Characteristic;
     this.currentDoorState = characteristic.CurrentDoorState.CLOSED;
     this.targetDoorState = this.currentDoorState;
 
-    this.platform.log.info('Doorstates:', this.currentDoorState, this.targetDoorState);
+    this.platform.log.info('Doorstates:', this.printState(this.currentDoorState), this.printState(this.targetDoorState));
     this.platform.log.debug('Openstate is:', characteristic.CurrentDoorState.OPEN);
 
     // set accessory information
@@ -58,34 +52,11 @@ export class GarageDoorOpener {
 
   }
 
-  // private SetupSocketIO(): Socket {
-  //   const socket = io(this.url);
-  //   this.platform.log.debug('Trying to connect ' + this.url);
-
-  //   socket.on('status', (message) => {
-  //     this.platform.log.debug('Status: ', message);
-  //     const state = message.status.garage === 'open' ?
-  //       this.platform.Characteristic.CurrentDoorState.OPEN : this.platform.Characteristic.CurrentDoorState.CurrentDoorState.CLOSED;
-
-  //     this.platform.log.info('Updating state from websocket ', message);
-  //     this.currentDoorState = state;
-  //     // this.service.updateCharacteristic(this.platform.Characteristic.CurrentDoorState, state);
-  //   });
-
-  //   socket.on('connect', () => {
-  //     this.platform.log.warn('SocketIO connected to ' + this.url);
-  //   });
-
-  //   setInterval(() => socket.emit('status'), 1000);
-
-  //   return socket;
-  // }
-
   private setupMqtt(): MqttClient {
     const mqtt = mqttjs.connect('mqtt://benchpress.local');
     mqtt.on('connect', () => {
       mqtt.publish('benchpress/homebridge-garageport', new Date().toUTCString(), { qos: 1, retain: true });
-      mqtt.subscribe(['garage/esp32/input/#', 'esp32garage'], (err) => {
+      mqtt.subscribe(['garage/esp32/input/#'], (err) => {
         if (err) {
           this.platform.log.error('MQTT subscription failed');
         }
@@ -93,19 +64,14 @@ export class GarageDoorOpener {
     });
 
     mqtt.on('message', async (topic, message) => {
-      if (topic.startsWith('esp32garage')) {
-        const esp32state = JSON.parse(message.toString());
-        const status = {
-          relayA: esp32state.relayA === '0',
-          relayB: esp32state.relayB === '0',
-          input1: esp32state.input1 === '0',
-          input2: esp32state.input2 === '0',
-        };
-        const state = status.input2 ?
+      // this.platform.log.debug('MQTT message received:', topic, message.toString());
+      if (topic.startsWith('garage/esp32/input/2/out')) {
+        const msg = message.toString();
+        const state = msg === '1' ?
           this.platform.Characteristic.CurrentDoorState.OPEN : this.platform.Characteristic.CurrentDoorState.CLOSED;
 
         this.currentDoorState = state;
-        this.platform.log.debug('Updating state from mqtt ', status, state);
+        this.platform.log.debug('Updating state from mqtt ', msg, this.printState(state));
         this.service.updateCharacteristic(this.platform.Characteristic.CurrentDoorState, state);
       }
     });
@@ -114,15 +80,15 @@ export class GarageDoorOpener {
   }
 
   async setCurrentDoorState(value: CharacteristicValue) {
-    this.platform.log.info('setCurrentDoorState ->', value);
+    this.platform.log.info('setCurrentDoorState ->', this.printState(value));
   }
 
   async setTargetDoorState(value: CharacteristicValue) {
-    this.platform.log.info('setTargetDoorState ->', value);
+    this.platform.log.info('setTargetDoorState ->', this.printState(value));
 
     this.targetDoorState = value;
-    this.platform.log.debug('Target state is now:', this.targetDoorState);
-    this.platform.log.debug('Current state is:', this.currentDoorState);
+    this.platform.log.debug('Target state is now:', this.printState(this.targetDoorState));
+    this.platform.log.debug('Current state is:', this.printState(this.currentDoorState));
     if (this.currentDoorState !== this.targetDoorState) {
       this.sendImpulseToMotor();
     }
@@ -130,22 +96,23 @@ export class GarageDoorOpener {
 
   async getCurrentDoorState(): Promise<CharacteristicValue> {
     const state = this.currentDoorState;
-    this.platform.log.debug('getCurrentDoorState ->', state);
+    this.platform.log.debug('getCurrentDoorState ->', this.printState(state));
     return state;
   }
 
   async getTargetDoorState(): Promise<CharacteristicValue> {
     const state = this.targetDoorState;
-    this.platform.log.debug('getTargetDoorState ->', state);
-
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    this.platform.log.debug('getTargetDoorState ->', this.printState(state));
     return state;
   }
 
   async sendImpulseToMotor() {
     this.platform.log.warn('websocket request to impulse motor. Publishing to mqtt');
     this.mqtt.publish('garage/esp32/in', 'G', { qos: 0, retain: false });
+  }
+
+  public printState(value:CharacteristicValue):string {
+    return value === 1 ? 'OPEN' : 'CLOSED';
   }
 
 }
